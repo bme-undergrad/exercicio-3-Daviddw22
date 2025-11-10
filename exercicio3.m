8c37d89e3e48a96e6bdb1b63527d39aa356cb606
David Drummond Wainstein <73289677+Daviddw22@users.noreply.github.com>
2025-11-10 18:32:28 -0300

    Update exercicio3.m

--- exercicio3.m
+++ exercicio3.m
@@ -5,13 +5,29 @@ function [mu] = exercicio3(t, NC)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-t_new = t-t(1);
-yi = log(NC);
-a = polyfit(t_new, yi, 1);
+% Garante que t comece em zero
+  t = t - t(1);
+
+  % Transforma os dados em log natural
+  Y = log(NC);
+
+  % Ajuste linear das primeiras duas horas
+  idx1 = find(t <= 2);
+  p1 = polyfit(t(idx1), Y(idx1), 1);
+  mu1 = p1(1);
+
+  % Ajuste linear das ultimas quatro horas 
+  idx2 = find(t >= 2.5);
+  p2 = polyfit(t(idx2), Y(idx2), 1);
+  mu2 = p2(1);
+
+  % Exibindo os resultados 
+  printf("Taxa de crescimento nas primeiras 2h: %.4f 1/h\n", mu1);
+  printf("Taxa de crescimento nas próximas 4h: %.4f 1/h\n", mu2);
 
 
 % mantenha essas duas linhas finais, subtituindo mu pelo valor que você calculou
-mu = a(1);
+mu = [mu1; mu2]
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
